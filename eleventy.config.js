3aa27d3bf78b9db3c70d3b4187a015b5a426fdbb
Amulya <amulya.aradhyula@gmail.com>
2026-02-08 21:14:53 -0800

    Revert "Add favicon and fix RSS feed metadata"

--- eleventy.config.js
+++ eleventy.config.js
@@ -76,11 +76,11 @@ export default async function(eleventyConfig) {
 		},
 		metadata: {
 			language: "en",
-			title: "Amulya Aradhyula",
-			subtitle: "Amulya's Web Homepage",
-			base: "https://amultiplied.com/",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
 			author: {
-				name: "Amulya Aradhyula"
+				name: "Your Name"
 			}
 		}
 	});
